b24a51bc972d839092bb7350e1b26da1b4004272
TTS기술담당 John berrybryan <skt.1902654@sk.com>
2017-10-26 17:50:54 +0900

    JSON and EnvVar configurations are working

--- SteamDataCollection/Program.cs
+++ SteamDataCollection/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace SteamDataCollection
 {
@@ -7,6 +9,19 @@ namespace SteamDataCollection
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var settings = new Settings();
+            configuration.Bind(settings);
+
+            Console.WriteLine($"ApplicationName is '{settings.ApplicationName}'");
+            Console.WriteLine($"ApiKey is '{settings.ApiKey}'");
         }
     }
 }
---
+++ SteamDataCollection/Settings.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace SteamDataCollection
+{
+    public class Settings
+    {
+        public String ApplicationName { get; set; }
+        public String ApiKey { get; set; }
+    }
+}
